58ecf84bff98e00b7273c11191ab853affd59a1e
burcar <zacharyburcar@gmail.com>
2021-12-05 23:07:17 -0500

    generated dummy readme file

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,71 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+const writeToFile = require('./utils/generateFile')
 
-// TODO: Create an array of questions for user input
-const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const promptUser = markdownData => {
+    if (!markdownData)  {
+        markdownData = [];
+    }
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true
+                } else {
+                    console.log('Please enter your Project Title!')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please enter a description of your project.'
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: "PLease enter your project's installation instructions."
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Please enter your projects usage information'
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: "Please enter your project's contribution guidelines"
+        },
+        {
+            type: 'input',
+            name: 'test',
+
+
+
+            message: "Please enter your project's test instructions"
+        }
+    ])
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    promptUser()
+    .then(markdownData => {
+        return generateMarkdown(markdownData)
+    })
+    .then(markdownData => {
+        return writeToFile("markdown", markdownData)
+    })
+    .catch(err => {
+        console.log(err)
+    })
+}
 
 // Function call to initialize app
 init();
